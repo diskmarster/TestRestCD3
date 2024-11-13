f1498c1e57d0931c597df44c2dff18406050f463
diskmarster <diskjen@gmail.com>
2024-11-13 11:22:55 +0100

    johnnytest1

--- TestRestCD3/Controllers/WeatherForecastController.cs
+++ TestRestCD3/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestRestCD3.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Skinke"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
